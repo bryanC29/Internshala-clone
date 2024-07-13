2728d67f51ddd65ba2453e636772db60f8575097
Bryan Christy <bryanchristy2016@gmail.com>
2024-07-13 10:39:51 +0530

    BREAKING: added language support in backend and frontend

--- src/server.js
+++ src/server.js
@@ -1,7 +1,42 @@
 const express = require("express");
 const connect = require("./configs/db");
+const i18n = require('i18n');
 const path = require("path");
 const app = express();
+const cookieParser = require('cookie-parser');
+
+i18n.configure({
+  locales: ['sp', 'hi', 'pr', 'ch', 'fr', 'en'],
+  directory: path.join(__dirname, 'locales'),
+  defaultLocale: 'en',
+  cookie: 'lang',
+  queryParameter: 'lang',
+  objectNotation: true,
+  register: global,
+  syncFiles: true,
+  getCookie: function(req) {
+    return req.cookies && req.cookies.lang ? req.cookies.lang : 'en';
+  }
+});
+
+app.use(cookieParser());
+
+app.get('/change-lang/:lang', (req, res) => {
+  const lang = ['sp', 'hi', 'pr', 'ch', 'fr', 'en'].includes(req.params.lang) ? req.params.lang : 'en';
+  res.cookie('lang', lang, { httpOnly: true });
+  res.redirect('back');
+});
+
+app.use((req, res, next) => {
+  if (!req.cookies || !req.cookies.lang) {
+    res.cookie('lang', 'en', {httpOnly: true });
+    req.cookies = req.cookies || {};
+    req.cookies.lang = 'en';
+  }
+  next();
+});
+
+app.use(i18n.init);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,7 +56,7 @@ const languages = ['en', 'hi', 'fr', 'de', 'es', 'zh'];
 const internshipController = require("./controllers/internship.controller");
 
 app.use((req, res, next) => {
-  const selectedLang = req.query.lang || 'en'; // Default to English
+  const selectedLang = req.query.lang || 'hi'; // Default to English
   if (!languages.includes(selectedLang)) {
     return res.status(400).send('Invalid language');
   }
@@ -34,6 +69,7 @@ app.use("/pages", pageController);
 app.use("/data", internshipController);
 
 app.get('/', (req, res) => {
+  locale: req.getLocale(),
   res.redirect(301, "/pages/index.ejs");
 })
 
